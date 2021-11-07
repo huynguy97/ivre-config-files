23138248f81418ed3dd115f7afa6b0ee5dc5b85f
Huy Nguyen <huy.nguy97@gmail.com>
2021-11-07 17:49:04 +0100

    Added example, fixed write to file.

--- scripts/export.py
+++ scripts/export.py
@@ -1,3 +1,6 @@
+# An export script for IVRE NDJSON files. Run as below:
+# python3 export.py [file] [searchterm]
+# example: "python3 export.py test.ndjson cve-2021" will write to a file
 import ndjson # Not a standard json library. Run "pip3 install ndjson" before running using the script. See: https://pypi.org/project/ndjson/
 import re
 import sys
@@ -11,7 +14,8 @@ with open(filename) as f:
     ips = ndjson.load(f)
 
 for i in range(len(ips)):
-    f = open("test.txt", "a")
+    output_file = sys.argv[1] + "_filtered"
+    f = open(output_file, "a")
     f.write(ips[i]['addr'] + "\t")
     ip = ndjson.dumps((ips[i].items()), indent=4)
     cleaned_ip = ip.replace("\\n", "\n")
