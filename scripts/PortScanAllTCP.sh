03f07f5ae5ca99d75fce693a755570b34acc480e
Huy Nguyen <huy.nguy97@gmail.com>
2021-11-08 21:41:43 +0100

    Update PortScanAllTCP.sh

--- scripts/PortScanAllTCP.sh
+++ scripts/PortScanAllTCP.sh
@@ -3,10 +3,12 @@
 # A word of warning. This file performs ALL TCP scans. So even those such as RDP, test-ssl and Nuclei that might 
 # end up freezing on some hosts. 
 # A simple solution would be to exclude these scans with an if statement and afterwards run them manually to check if they freeze.
+# Another trick that might be useful is to change the starting point. The first 6 scans in /etc/ivre.conf are "problematic" (see readme)
+# thus changing i=0 to i=6 will skip these scans so they can ran manually. 
 
 tcp_targets=($(grep -B 4 'scans\s*=\s*"SV"' /etc/ivre.conf | grep 'NMAP_SCAN_TEMPLATES' | grep '".*"' -o | sed 's/.//;s/.$//'))
 length_tcp=${#tcp_targets[@]} # list of nmap scan templates
-for ((i=0; i != length_tcp; i++)) do
+for ((i=0; i != length_tcp; i++)) do # change i=0 to i=6 to skip the problematic scans. 
         temporary=${tcp_targets[i]} 
         printf "t\n%s\ny" "$temporary" | ./PortScan.sh
 done
